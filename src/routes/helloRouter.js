09d3dbefdd7550f46724fcbde6465a602f961fea
Oleksandr Hushcha <58427288+RisingCthulhu@users.noreply.github.com>
2023-09-26 18:47:41 +0300

    Changed "/" route to "/values" that returns paginated values in helloRouter.js

--- src/routes/helloRouter.js
+++ src/routes/helloRouter.js
@@ -2,8 +2,16 @@ import { Router } from "express";
 
 const routes = Router();
 
-routes.get("/", async (req, res) => {
-  res.status(200).send({ message: "Hello World!" });
+const values = [...Array(6400)].map(() => ({ value: Math.floor(Math.random() * 100) }));
+
+routes.get("/values", async (req, res) => {
+  const perPage = Number.parseInt(req.perPage, 10) || 50;
+  const page = Number.parseInt(req.cursor, 10) || 1;
+  
+  res.status(200).send({
+    values: values.slice(perPage * (page - 1), perPage * page),
+    cursor: (page + 1).toString(10)
+  });
 });
 
 export default routes;
