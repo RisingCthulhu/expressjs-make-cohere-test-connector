7c4b73a20c072b68db82119f554b7e0dc138b75a
Oleksandr Hushcha <58427288+RisingCthulhu@users.noreply.github.com>
2023-09-27 08:54:29 +0300

    Update helloRouter.js

--- src/routes/helloRouter.js
+++ src/routes/helloRouter.js
@@ -7,10 +7,13 @@ const values = [...Array(6400)].map(() => ({ value: Math.floor(Math.random() * 1
 routes.get("/values", async (req, res) => {
   const perPage = Number.parseInt(req.query.perPage, 10) || 50;
   const page = Number.parseInt(req.query.cursor, 10) || 1;
+  const currentPageValues = values.slice(perPage * (page - 1), perPage * page);
+  const nextPage = page + 1;
+  const nextPageValues = values.slice(perPage * (nextPage - 1), perPage * nextPage);
   
   res.status(200).send({
     values: values.slice(perPage * (page - 1), perPage * page),
-    cursor: (page + 1).toString(10)
+    cursor: nextPageValues.length !== 0 ? (page + 1).toString(10) : undefined
   });
 });
 
