fa181a3fd3006d9735b03ed2abb9b2736ffa9c19
o-hushcha <o.hushcha.ext@make.com>
2025-06-05 23:11:08 +0300

    replace http server with serverless-http handler in the index.js

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,15 +1,18 @@
 import http from "http";
 import { config } from "dotenv";
+import serverless from "serverless-http";
 import app from "./app.js";
 import * as logger from "./utils/logger.js";
 
 if (process.env.NODE_ENV !== "production") {
   config();
 }
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3003;
+// const PORT = process.env.PORT || 3003;
 
-server.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
-});
+// server.listen(PORT, () => {
+//   logger.info(`Server running on port ${PORT}`);
+// });
+
+export const handler = serverless(app);
