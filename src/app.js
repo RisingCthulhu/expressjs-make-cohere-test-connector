9de1bf6f11f23cbae9ffe991bbc0985c146352c0
o-hushcha <o.hushcha.ext@make.com>
2025-06-05 20:17:13 +0300

    add new "/resend-to-wh" route that responds with 307 HTTP code and redirects to the webhook.site WH URL

--- src/app.js
+++ src/app.js
@@ -23,6 +23,10 @@ app.get('/', (req, res) => {
     res.status(200).send({ status: 'ok' });
 });
 
+app.post('/resend-to-wh', (req, res) => {
+  res.redirect(307, 'https://webhook.site/a2f457b5-5f51-4c59-a2ea-0f9b36113e2b')
+})
+
 app.use('/hello', helloRoute);
 app.use('/philosophers', philosophersRouter);
 app.use('/gCalendarMock', gCalendarMockRouter);
