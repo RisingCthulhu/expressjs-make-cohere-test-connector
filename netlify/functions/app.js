685906c59d67bce2fdc95db115d6ddc62eef91f0
o-hushcha <o.hushcha.ext@make.com>
2025-06-05 23:50:51 +0300

    fix actual status codes sent

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -24,11 +24,11 @@ app.get('/', (req, res) => {
 });
 
 app.post('/301', (req, res) => {
-  res.redirect(307, 'https://webhook.site/a2f457b5-5f51-4c59-a2ea-0f9b36113e2b')
+  res.redirect(301, 'https://webhook.site/a2f457b5-5f51-4c59-a2ea-0f9b36113e2b')
 })
 
 app.post('/302', (req, res) => {
-  res.redirect(307, 'https://webhook.site/a2f457b5-5f51-4c59-a2ea-0f9b36113e2b')
+  res.redirect(302, 'https://webhook.site/a2f457b5-5f51-4c59-a2ea-0f9b36113e2b')
 })
 
 app.post('/307', (req, res) => {
