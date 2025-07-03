2cd21bd2aa39d8d84a2a1be9a205cef39b456cfe
o-hushcha <o.hushcha.ext@make.com>
2025-07-03 18:27:50 +0300

    fix "unknown endpoint" msg in unknownEndpoint middleware

--- netlify/functions/utils/middleware.js
+++ netlify/functions/utils/middleware.js
@@ -1,7 +1,7 @@
 import "./logger.js";
 
 export const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: `uknonwn endpoint` });
+  res.status(404).send({ error: `unknown endpoint` });
 };
 
 export const errorHandler = (error, req, res, next) => {
